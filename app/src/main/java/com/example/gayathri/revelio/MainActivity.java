64e2ffd858fd13295aaa2acbdf48521ce6fee06e
gayathriksoman <gayathrikoooniyath@gmail.com>
2017-02-06 15:26:04 +0530

    frontend 2. added map fragments

--- app/src/main/java/com/example/gayathri/revelio/MainActivity.java
+++ app/src/main/java/com/example/gayathri/revelio/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gayathri.revelio;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -77,6 +80,9 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
+
+        android.app.FragmentManager fm=null;
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -85,12 +91,9 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
+            fm.beginTransaction().replace(R.id.content_main, new Maps()).commit();
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
--- app/src/main/java/com/example/gayathri/revelio/Maps.java
+++ app/src/main/java/com/example/gayathri/revelio/Maps.java
@@ -1,8 +1,50 @@
 package com.example.gayathri.revelio;
 
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by gayathri on 5/2/17.
  */
 
-public class Maps {
+public class Maps extends Fragment implements OnMapReadyCallback{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+         return inflater.inflate(R.layout.fragment_gmap, container, false);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng marker = new LatLng(33,44);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 13));
+
+        googleMap.addMarker(new MarkerOptions().title("NITC maps").position(marker));
+
+    }
 }
