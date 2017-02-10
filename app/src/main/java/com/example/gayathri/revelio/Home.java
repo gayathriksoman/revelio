9c2b50274079f2b3e0e5247caf9e6e1c160f7d04
reshmi810 <reshmi.sriram810@gmail.com>
2017-02-10 21:18:32 +0530

    Front end remade

--- app/src/main/java/com/example/gayathri/revelio/Home.java
+++ app/src/main/java/com/example/gayathri/revelio/Home.java
@@ -2,6 +2,7 @@ package com.example.gayathri.revelio;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
 
@@ -11,11 +12,14 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setLogo(R.mipmap.revelio_icon);
+        setSupportActionBar(myToolbar);
     }
 
     public void maps(View view)
     {
-         Intent i=new Intent(this,Maps.class);
+         Intent i=new Intent(this,MapsActivity.class);
         startActivity(i);
     }
 }
